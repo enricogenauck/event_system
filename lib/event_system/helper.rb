22be73b22943fa2e3dd9edaf37762a857264624a
cr0 <cr0@lunar_c2d.(none)>
2010-06-21 12:12:46 +0200

    changed helper string order because of a bug in rails 2.3.7&2.3.8

--- lib/event_system/helper.rb
+++ lib/event_system/helper.rb
@@ -5,13 +5,13 @@ module EventSystem
         url = url_for :controller => controller_name, :action => "updates_for_#{action_name}"
         interval = controller.event_system_update_interval
 
+        "<span id='event_system_last_load' style='display: none;'>#{@controller.current_event_number = EventSystem::IndicatorSequence.current}</span>"+
         javascript_tag(
           "function getNewEvents(url) {
              url = url+'?last_load='+$('#event_system_last_load').text();
              $.getScript(url);
            };
            setInterval(function() {getNewEvents('#{url}');}, #{interval*1000});")+
-          "<span id='event_system_last_load' style='display: none;'>#{@controller.current_event_number = EventSystem::IndicatorSequence.current}</span>"
       else
         ""
       end
